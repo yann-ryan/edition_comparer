4a40a2ba6fad8e7b7ed5275fae066ae952258d12
yannryan <yann.ryan@gmail.com>
2022-11-14 14:55:14 +0200

    added app

---
+++ app.R
@@ -0,0 +1,207 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(knitr)
+library(here)
+library(tidyverse)
+library(keyring)
+library(gt)
+library(DBI)
+library(RMariaDB)
+library(IRanges)
+library(plotly)
+options(scipen = 999999)
+
+con <- dbConnect(
+  drv = MariaDB(),
+  host = "vm3065.kaj.pouta.csc.fi",
+  dbname = "hpc-hd",
+  user = Sys.getenv("DB_USER") ,
+  password =  Sys.getenv("DB_PASS"),
+  bigint = "integer",
+  load_data_local_infile = TRUE,
+  autocommit = TRUE,
+  reconnect = TRUE
+)
+
+
+get_ranges = function(ecco_id, id, reuse_df){
+  tryCatch({
+    ra = reuse_df %>% 
+      filter(ecco_id.x == id & ecco_id.y == ecco_id) %>% 
+      arrange(t2_start, t2_end) %>% select(start = t2_start, end = t2_end)
+    
+    ir1 = IRanges(start = ra$start, end = ra$end)
+    
+    islands = ir1 %>% 
+      as.data.frame() %>% 
+      as_tibble() %>% 
+      mutate(type = 'island')
+    
+    gaps = gaps(ir1) %>% 
+      as.data.frame() %>% 
+      as_tibble() %>% 
+      mutate(type = 'gap')
+    
+    rbind(islands, gaps) %>% 
+      mutate(doc_id = ecco_id)
+    
+    
+  }, error = function(e) {
+    
+  })
+  
+}
+
+idmap <- tbl(con,'idmap',dbplyr::in_schema("hpc-hd","idmap"))
+full_reuses <- tbl(con,dbplyr::in_schema("hpc-hd","textreuses_2d"))
+estc_core = tbl(con,dbplyr::in_schema("hpc-hd","estc_core"))
+
+
+
+
+ui <- fluidPage(
+  
+  
+  titlePanel("Edition Comparer"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("bins", "Work ID:", "2179-fable of bees or private vices public benefits"),
+      actionButton('click', 'Generate'),
+      uiOutput('compare'),
+      plotlyOutput("distPlot")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(tags$head(
+      tags$style(
+        HTML("width: 1000px"))),
+      htmlOutput("frame")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  
+  output$compare = renderUI({
+    
+    withProgress(message = 'Generating editions from work ID', value = 0, {
+      
+      x = input$bins
+      
+      work_ids_df = idmap %>% filter(work_id == x) %>% 
+        inner_join(estc_core %>% 
+                     select(estc_id, publication_year)) %>% mutate(publication_year = as.integer(publication_year)) %>% 
+        #mutate(publication_year = paste0(ecco_id, " (", publication_year, ")")) %>% 
+        arrange(publication_year) %>% 
+        select(ecco_id, publication_year)
+      
+      work_ids = work_ids_df %>% pull(ecco_id)
+      names(work_ids) = work_ids_df %>% pull(publication_year)
+      
+      selectInput('which_to_compare', 'Seed: ', choices = work_ids)
+    }) 
+  })
+  
+  hypo = eventReactive(input$click, {
+    withProgress(message = 'Gathering reuses', value = 0, {
+      
+      i = input$bins
+      
+      all_ids = estc_core %>% 
+        filter(work_id == i) %>% 
+        left_join(idmap, by = 'estc_id') %>% 
+        arrange(publication_year) %>% filter(!is.na(ecco_id)) %>%  
+        pull(ecco_id)
+      
+      x = input$which_to_compare  
+      
+      ids = idmap %>% filter(ecco_id %in% all_ids)
+      
+      first_id = idmap %>% filter(ecco_id == x)
+      
+      r1 = full_reuses %>% 
+        inner_join(first_id, by = c('t1_id' = 't_id'))%>% 
+        inner_join(ids, by = c('t2_id' = 't_id')) %>% 
+        left_join(estc_core %>% select(estc_id, publication_year, short_title), by= c('estc_id.y' = 'estc_id')) %>% 
+        collect()
+      
+      r2 = r1 %>% select( -publication_year, -short_title)
+      
+      
+      
+      y = all_ids[! all_ids == x]
+      
+      withProgress(message = 'Calculating Missing Ranges', value = 0, {
+        
+        t = lapply(y, get_ranges, id = x, reuse_df = r2) %>% 
+          data.table::rbindlist() %>% 
+          mutate(gap_link_text = paste0("<a href='https://a3s.fi/octavo-reader/index.html#?doc=",doc_id,"&startOffset=",start,"&endOffset=", end, "'>", doc_id, "</a><br>"))%>% 
+          mutate(gap_link = paste0("https://a3s.fi/octavo-reader/index.html#?doc=",doc_id,"&startOffset=",start,"&endOffset=", end)) %>% 
+          mutate(gap_link = paste0(gap_link, "&doc=", x, "&startOffset=",start,"&endOffset=", end)) %>%
+          left_join(r1 %>% 
+                      distinct(ecco_id.y, short_title, publication_year, work_id.y), by = c('doc_id' = 'ecco_id.y'))
+        
+        
+        t
+      })
+      
+    })
+    
+  })
+  
+  
+  
+  output$distPlot <- renderPlotly({
+    
+    req(hypo())
+    
+    p = hypo()  %>% filter(type == 'gap') %>% 
+      arrange(desc(publication_year)) %>% 
+      mutate(doc_id = factor(doc_id, levels = unique(doc_id))) %>% 
+      arrange(work_id.y, publication_year) %>% ungroup() %>% 
+      #mutate(sort_level = 1:nrow(.)) %>% 
+      ggplot(aes(text = paste0(short_title, " (", publication_year, ")"))) + 
+      geom_segment(aes( color =type, x = start, xend = end, y = doc_id, yend = doc_id), size = 2) + theme_void() + theme(legend.position = 'none') 
+    
+    
+    
+    gp = plotly::ggplotly(p)
+    
+    gp%>%
+      event_register("plotly_selecting")
+    
+  })
+  
+  
+  src = reactive({
+    req(event_data("plotly_click"))
+    d <- event_data("plotly_click")
+    i = as.numeric(d['x'])
+    
+    hypo() %>% filter(type == 'gap') %>% filter(end == i) %>% head(1) %>% 
+      pull(gap_link)
+  })
+  
+  
+  output$frame <- renderUI({
+    
+    my_test <- tags$iframe(src=src(), height=600, width=1200)
+    print(my_test)
+    my_test
+    
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
